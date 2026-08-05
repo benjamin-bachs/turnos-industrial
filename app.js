b404f6c0a1a405037839a1a02f4b9347e1d5cbb4
benjamin-bachs <84108302+benjamin-bachs@users.noreply.github.com>
2026-08-05 14:47:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+import { supabase } from "./supabase.js";
 
+async function cargarTurnos() {
+
+    const { data, error } = await supabase
+        .from("inscripciones")
+        .select("*");
+
+    if(error){
+        console.error(error);
+        return;
+    }
+
+    console.log(data);
+}
