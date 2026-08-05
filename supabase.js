75b51d17a49f188fd5e50f2267a0d4fa821e5fcc
benjamin-bachs <84108302+benjamin-bachs@users.noreply.github.com>
2026-08-05 14:52:09 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
 import { SUPABASE_URL, SUPABASE_KEY } from "./config.js";
 
 export const supabase = createClient(
